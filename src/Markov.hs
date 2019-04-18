2e4777dbe7e67df3dc62b71af94694662d950649
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-04-17 23:00:07 -0400

    Adds a Markov Chain library, adds a first function to get a suffixes array

---
+++ src/Markov.hs
@@ -0,0 +1,27 @@
+{- |
+Module      :   Markov
+Description :   Basic implementation of a Markov Chain algorithm for text
+Copyright   :   (c) Andrés Ignacio Torres
+License     :   MIT
+Maintainer  :   andresitorresm@gmail.com
+Stability   :   experimental
+
+Basic implementation of an arbitrary-length Markov Chain algorithm.
+-}
+
+{-|
+    Given an array of strings (words) and a context length,
+    returns an array with each possible prefix (as an array) and the
+    corresponding suffix.
+-}
+getSuffixArray :: [String] -> Int -> [([String], String)]
+getSuffixArray [] _ = []
+getSuffixArray words n =
+  let enoughWords = length words > n in
+    if enoughWords then
+      let prefix = take n words
+        suffix = words !! (n)
+        remainingWords = tail words
+      in  (prefix, suffix) : getSuffixArray remainingWords n
+    else []
+
