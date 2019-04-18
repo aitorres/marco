36c7165079536fe2b914a3a9ceb5f16c16390784
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-04-18 03:48:46 -0400

    Improves efficiency and reports execution time

--- src/Main.hs
+++ src/Main.hs
@@ -34,10 +34,10 @@ getRandomElement x = do
   Procedurally generates a new phrase, given base string encoded
   as a Phrase, an initial prefix and an approx. max length
 -}
-getNewPhrase :: Phrase -> [String] -> String -> Int -> IO (String)
-getNewPhrase phrase first_prefix str max = do
-  putStrLn $ "so far, you've got " ++ show (length str) ++ " words"
-  let suffixes = getPrefixSuffixes phrase first_prefix
+getNewPhrase :: [MarkovToken] -> Phrase -> [String] -> String -> Int -> IO (String)
+getNewPhrase toks phrase first_prefix str max = do
+  putStrLn $ "so far, you've got: " ++ str
+  let suffixes = getPrefixSuffixes' toks first_prefix
   case length suffixes of
     0 -> do
       return str
@@ -48,7 +48,7 @@ getNewPhrase phrase first_prefix str max = do
         otherwise -> do
           suffix <- getRandomElement suffixes
           let new_prefix = (tail first_prefix) ++ [suffix]
-          getNewPhrase phrase new_prefix (str ++ " " ++ suffix) (max - 1)
+          getNewPhrase toks phrase new_prefix (str ++ " " ++ suffix) (max - 1)
 
 printHelp :: IO ()
 printHelp = do
@@ -61,26 +61,50 @@ checkArgs _ = do
   printHelp
   exitFailure
 
+properPrintLn :: String -> IO ()
+properPrintLn str = do
+  putStrLn str
+  hFlush stdout
+
 {-|
   Main client for the application.
 -}
 main :: IO ()
 main = do
+  -- Validates arguments (or exits with a help message if they are invalid)
   args <- getArgs
   checkArgs args
+
+  -- Parses arguments
   let filename = args !! 0
   let raw_n = args !! 1
   let raw_max = args !! 2
-  inputPhrase <- readFile filename
   let n = read raw_n :: Int
   let max = read raw_max :: Int
+
+  -- Reads filename
+  file_read_start_time <- getPOSIXTime
+  inputPhrase <- readFile filename
+  file_read_finish_time <- getPOSIXTime
+  properPrintLn $ "File properly loaded into memory after " ++ (show (file_read_finish_time - file_read_start_time))  ++ "\n"
+
+  -- Pre-processes the data
+  properPrintLn $ "Reading file's content and pre-processing into proper data structures"
+  pre_proc_start_time <- getPOSIXTime
   let phrase = stringToPhrase inputPhrase
-  let prefixes = getPhrasePrefixes phrase n
+  let tokens = getPhraseTokens phrase n
+  let prefixes = getPhrasePrefixes' tokens
   first_prefix <- getRandomElement prefixes
-  putStrLn $ "Phrase generation: ENGAGED"
-  start_time <- getPOSIXTime
-  nphrase <- getNewPhrase phrase first_prefix [] max
-  finish_time <- getPOSIXTime
-  putStrLn $ "Phrase generation: COMPLETED"
-  putStrLn $ "Time spent: " ++ (show (finish_time - start_time)) ++ "ms"
-  putStrLn $ "Your phrase is: " ++ nphrase
+  pre_proc_finish_time <- getPOSIXTime
+  properPrintLn $ "Time spent (pre-processing): " ++ (show (pre_proc_finish_time - pre_proc_start_time)) ++ "\n"
+
+  -- Generates the new text
+  properPrintLn $ "Phrase generation: ENGAGED"
+  proc_start_time <- getPOSIXTime
+  nphrase <- getNewPhrase tokens phrase first_prefix [] max
+  proc_finish_time <- getPOSIXTime
+  properPrintLn $ "Phrase generation: COMPLETED"
+  properPrintLn $ "Time spent (generation): " ++ (show (proc_finish_time - proc_start_time)) ++ "\n"
+
+  -- Reports the text and exits
+  properPrintLn $ "Your phrase is: " ++ nphrase
