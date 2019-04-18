b0bfacaf81efee5e55a6186253def5f476c15cf7
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-04-18 00:02:05 -0400

    Adds functions to preprocess a phrase into an array of words

--- src/Markov.hs
+++ src/Markov.hs
@@ -11,6 +11,10 @@ Basic implementation of an arbitrary-length Markov Chain algorithm.
 
 module Markov where
 
+import Data.Char
+import Data.List.Split
+
+
 {-|
   Given an array of strings (words) and a context length,
   returns an array with each possible prefix (as an array) and the
@@ -41,10 +45,35 @@ compactSuffixArray arr =
         | otherwise = x : dropDupsAux xs (x:seen)
   in  dropDuplicates denseSuffixArray
 
-{-
+{-|
   Given an array of pairs with prefixes and a suffix for each one,
   and a given prefix, returns an array of suffixes that correspond to such prefix. 
 -}
 findAllSuffixes :: [([String], String)] -> [String] -> [String]
 findAllSuffixes arr prefix =
   [b | (a, b) <- arr, a == prefix]
+
+{-|
+  Normalizes a given word with the following criteria:
+  * The word must begin with an alphanumeric character
+  * The word must end with an alphanumeric character
+  * The word must be all in lowercase
+-}
+normalize :: String -> String
+normalize =
+  let lowerize = map toLower
+      removeStartSymbols = dropWhile (not . isAlpha)
+      removeEndSymbols = reverse . removeStartSymbols . reverse
+      removeSymbols = removeStartSymbols . removeEndSymbols
+  in  lowerize . removeSymbols
+
+{-|
+  Given a phrase as a string, separates all the tokens into single,
+  normalized words, and returns them in an array.
+-}
+splitToArray :: String -> [String]
+splitToArray [] = []
+splitToArray phrase =
+  let wordStart = dropWhile (not . isAlpha) phrase
+      (word, remainingPhrase) = span isAlpha wordStart
+  in  (normalize word) : (splitToArray remainingPhrase)
