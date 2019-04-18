dcdd9ff6303c89b521a39b639e2b40bd2f2d1340
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-04-18 01:38:37 -0400

    Refactors into custom data types

--- src/Markov.hs
+++ src/Markov.hs
@@ -9,49 +9,27 @@ Stability   :   experimental
 Basic implementation of an arbitrary-length Markov Chain algorithm.
 -}
 
-module Markov where
+module Markov (
+  stringToPhrase,
+  getPhraseTokens,
+  getPhrasePrefixes,
+  getPrefixSuffixes
+) where
 
 import Data.Char
 import Data.List.Split
 
+data Phrase =
+  Phrase [String]
+  deriving (Eq, Show)
 
-{-|
-  Given an array of strings (words) and a context length,
-  returns an array with each possible prefix (as an array) and the
-  corresponding suffix.
--}
-getSuffixArray :: [String] -> Int -> [([String], String)]
-getSuffixArray [] _ = []
-getSuffixArray words n
-  | length words > n =
-    let prefix = take n words
-        suffix = words !! (n)
-        remainingWords = tail words
-    in  (prefix, suffix) : getSuffixArray remainingWords n
-  | otherwise = []
+data DisjointMarkovToken =
+  DisjointMarkovToken ([String], String)
+  deriving (Eq, Show)
 
-{-|
-  Given a suffix array, "compresses" the array in order to find common prefixes
-  and associate all of the suffixes to them.
--}
-compactSuffixArray :: [([String], String)] -> [([String], [String])]
-compactSuffixArray arr =
-  let denseSuffixArray = [(prefix, findAllSuffixes arr prefix) | (prefix, _) <- arr]
-      dropDuplicates x =
-        dropDupsAux x []
-      dropDupsAux [] _ = []
-      dropDupsAux (x:xs) seen
-        | elem x seen = dropDupsAux xs seen
-        | otherwise = x : dropDupsAux xs (x:seen)
-  in  dropDuplicates denseSuffixArray
-
-{-|
-  Given an array of pairs with prefixes and a suffix for each one,
-  and a given prefix, returns an array of suffixes that correspond to such prefix. 
--}
-findAllSuffixes :: [([String], String)] -> [String] -> [String]
-findAllSuffixes arr prefix =
-  [b | (a, b) <- arr, a == prefix]
+data MarkovToken =
+  MarkovToken ([String], [String])
+  deriving (Eq, Show)
 
 {-|
   Normalizes a given word with the following criteria:
@@ -77,3 +55,66 @@ splitToArray phrase =
   let wordStart = dropWhile (not . isAlpha) phrase
       (word, remainingPhrase) = span isAlpha wordStart
   in  (normalize word) : (splitToArray remainingPhrase)
+
+{-|
+  Given a phrase as a string, returns a proper Phrase instance.
+-}
+stringToPhrase :: String -> Phrase
+stringToPhrase x = Phrase (splitToArray x)
+
+{-|
+  Given a phrase, returns an array of MarkovTokens with all the prefixes
+  and all of their suffixes
+-}
+getPhraseTokens :: Phrase -> [MarkovToken]
+getPhraseTokens x = compactSuffixArray $ getSuffixArray x 2
+
+{-|
+  Given a phrase, returns an array with arrays of strings, each with the possible prefixes.
+-}
+getPhrasePrefixes :: Phrase -> [[String]]
+getPhrasePrefixes x = [y | (MarkovToken (y, _)) <- getPhraseTokens x]
+
+{-|
+  Given a phrase and a prefix, returns such prefix suffixes if they exist.
+-}
+getPrefixSuffixes :: Phrase -> [String] -> [String]
+getPrefixSuffixes phrase prefix = [x | y <- [b | MarkovToken (a, b) <- getPhraseTokens phrase, a == prefix], x <- y]
+
+{-|
+  Given an array of strings (words) and a context length,
+  returns an array with each possible prefix (as an array) and the
+  corresponding suffix.
+-}
+getSuffixArray :: Phrase -> Int -> [DisjointMarkovToken]
+getSuffixArray (Phrase []) _ = []
+getSuffixArray (Phrase words) n
+  | length words > n =
+    let prefix = take n words
+        suffix = words !! n
+        remainingWords = tail words
+    in  (DisjointMarkovToken (prefix, suffix)) : getSuffixArray (Phrase remainingWords) n
+  | otherwise = []
+
+{-|
+  Given a suffix array, "compresses" the array in order to find common prefixes
+  and associate all of the suffixes to them.
+-}
+compactSuffixArray :: [DisjointMarkovToken] -> [MarkovToken]
+compactSuffixArray arr =
+  let denseSuffixArray = [(MarkovToken (prefix, findAllSuffixes arr prefix)) | (DisjointMarkovToken (prefix, _)) <- arr]
+      dropDuplicates x =
+        dropDupsAux x []
+      dropDupsAux [] _ = []
+      dropDupsAux (x:xs) seen
+        | elem x seen = dropDupsAux xs seen
+        | otherwise = x : dropDupsAux xs (x:seen)
+  in  dropDuplicates denseSuffixArray
+
+{-|
+  Given an array of pairs with prefixes and a suffix for each one,
+  and a given prefix, returns an array of suffixes that correspond to such prefix.
+-}
+findAllSuffixes :: [DisjointMarkovToken] -> [String] -> [String]
+findAllSuffixes (arr) prefix =
+  [b | (DisjointMarkovToken (a, b)) <- arr, a == prefix]
